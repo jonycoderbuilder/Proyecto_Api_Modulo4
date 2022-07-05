d0d74682810a88f732067507ce2476e83cc8945c
jonathan <jonraam11@yahoo.com>
2022-07-05 14:34:54 -0500

    actualizaciones

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,25 @@
-console.log('hola');
+import axios from 'axios';
+
+const firstDiv = document.getElementById("myDiv")
+
+async function getUser() {
+    try {
+        const { data } = await axios.get('https://62aa3cf53b31438554448672.mockapi.io/users');
+        const users = data.map((user) => {
+            return `
+            <div class="Hi">
+                <div class="img">
+                    <img src="${user.avatar}" />
+                </div>
+                <div class="Nom">
+                    <p>${user.name}</p>
+                </div>
+            </div>`
+        })
+        firstDiv.innerHTML = users;
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+getUser();
